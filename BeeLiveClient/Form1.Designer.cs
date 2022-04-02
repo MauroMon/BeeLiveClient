9efbc1882f42d860bd8d4cbc9d25c50ff09adc26
m.monteneri <m.monteneri@loccioni.com>
2022-04-02 15:59:08 +0200

    add logic to get hive status

--- BeeLiveClient/Form1.Designer.cs
+++ BeeLiveClient/Form1.Designer.cs
@@ -29,20 +29,30 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.timerNoise = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.numericUpDownMin = new System.Windows.Forms.NumericUpDown();
             this.numericUpDownMax = new System.Windows.Forms.NumericUpDown();
             this.buttonStartStop = new System.Windows.Forms.Button();
+            this.textBoxValue = new System.Windows.Forms.TextBox();
+            this.labelValue = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelNoise = new System.Windows.Forms.Label();
+            this.textBoxHiveNoise = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panelColor = new System.Windows.Forms.Panel();
+            this.timerHive = new System.Windows.Forms.Timer(this.components);
+            this.buttonCheckStart = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMin)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMax)).BeginInit();
             this.SuspendLayout();
             // 
-            // timer
+            // timerNoise
             // 
-            this.timer.Interval = 10000;
-            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            this.timerNoise.Interval = 10000;
+            this.timerNoise.Tick += new System.EventHandler(this.timer_Tick);
             // 
             // label1
             // 
@@ -96,11 +106,105 @@
             this.buttonStartStop.UseVisualStyleBackColor = true;
             this.buttonStartStop.Click += new System.EventHandler(this.buttonStartStop_Click);
             // 
+            // textBoxValue
+            // 
+            this.textBoxValue.Location = new System.Drawing.Point(51, 100);
+            this.textBoxValue.Name = "textBoxValue";
+            this.textBoxValue.ReadOnly = true;
+            this.textBoxValue.Size = new System.Drawing.Size(100, 23);
+            this.textBoxValue.TabIndex = 5;
+            // 
+            // labelValue
+            // 
+            this.labelValue.AutoSize = true;
+            this.labelValue.Location = new System.Drawing.Point(15, 103);
+            this.labelValue.Name = "labelValue";
+            this.labelValue.Size = new System.Drawing.Size(35, 15);
+            this.labelValue.TabIndex = 6;
+            this.labelValue.Text = "Value";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(17, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Sensor simulation";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(315, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Hive status";
+            // 
+            // labelNoise
+            // 
+            this.labelNoise.AutoSize = true;
+            this.labelNoise.Location = new System.Drawing.Point(315, 44);
+            this.labelNoise.Name = "labelNoise";
+            this.labelNoise.Size = new System.Drawing.Size(37, 15);
+            this.labelNoise.TabIndex = 10;
+            this.labelNoise.Text = "Noise";
+            // 
+            // textBoxHiveNoise
+            // 
+            this.textBoxHiveNoise.Location = new System.Drawing.Point(358, 41);
+            this.textBoxHiveNoise.Name = "textBoxHiveNoise";
+            this.textBoxHiveNoise.ReadOnly = true;
+            this.textBoxHiveNoise.Size = new System.Drawing.Size(100, 23);
+            this.textBoxHiveNoise.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(315, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Status";
+            // 
+            // panelColor
+            // 
+            this.panelColor.Location = new System.Drawing.Point(360, 73);
+            this.panelColor.Name = "panelColor";
+            this.panelColor.Size = new System.Drawing.Size(98, 26);
+            this.panelColor.TabIndex = 12;
+            // 
+            // timerHive
+            // 
+            this.timerHive.Interval = 5000;
+            this.timerHive.Tick += new System.EventHandler(this.timerHive_Tick);
+            // 
+            // buttonCheckStart
+            // 
+            this.buttonCheckStart.Location = new System.Drawing.Point(464, 41);
+            this.buttonCheckStart.Name = "buttonCheckStart";
+            this.buttonCheckStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonCheckStart.TabIndex = 13;
+            this.buttonCheckStart.Text = "Start";
+            this.buttonCheckStart.UseVisualStyleBackColor = true;
+            this.buttonCheckStart.Click += new System.EventHandler(this.buttonCheckStart_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(576, 145);
+            this.Controls.Add(this.buttonCheckStart);
+            this.Controls.Add(this.panelColor);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.labelNoise);
+            this.Controls.Add(this.textBoxHiveNoise);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelValue);
+            this.Controls.Add(this.textBoxValue);
             this.Controls.Add(this.buttonStartStop);
             this.Controls.Add(this.numericUpDownMax);
             this.Controls.Add(this.numericUpDownMin);
@@ -117,11 +221,21 @@
 
         #endregion
 
-        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Timer timerNoise;
         private Label label1;
         private Label label2;
         private NumericUpDown numericUpDownMin;
         private NumericUpDown numericUpDownMax;
         private Button buttonStartStop;
+        private TextBox textBoxValue;
+        private Label labelValue;
+        private Label label3;
+        private Label label4;
+        private Label labelNoise;
+        private TextBox textBoxHiveNoise;
+        private Label label5;
+        private Panel panelColor;
+        private System.Windows.Forms.Timer timerHive;
+        private Button buttonCheckStart;
     }
 }
--- BeeLiveClient/Form1.cs
+++ BeeLiveClient/Form1.cs
@@ -17,33 +17,96 @@ namespace BeeLiveClient
             };
             client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/json"));
-            random = new Random(); 
+            random = new Random();
         }
 
         private async void timer_Tick(object sender, EventArgs e)
         {
-            var seed = Convert.ToDecimal(random.NextDouble());
-            var decibel = numericUpDownMin.Value + (seed * (numericUpDownMax.Value - numericUpDownMin.Value));
-            var noiseData = new BeeLive.NoiseData.TransferModels.NoiseDataDto()
+            try
             {
-                HiveId = 1,
-                Decibel = decibel
-            };
-            var result = await client.PostAsJsonAsync(ConfigurationManager.AppSettings["NoiseDataUrl"], noiseData);
+                var seed = Convert.ToDecimal(random.NextDouble());
+                var decibel = numericUpDownMin.Value + (seed * (numericUpDownMax.Value - numericUpDownMin.Value));
+                var noiseData = new BeeLive.NoiseData.TransferModels.NoiseDataDto()
+                {
+                    HiveId = 1,
+                    Decibel = decibel
+                };
+                var result = await client.PostAsJsonAsync(ConfigurationManager.AppSettings["NoiseDataUrl"], noiseData);
+                if (result.IsSuccessStatusCode)
+                {
+                    textBoxValue.Text = decibel.ToString();
+                }
+                else
+                {
+                    textBoxValue.Text = $"ERROR {result.StatusCode.ToString()}";
+                }
 
+            }
+            catch (Exception ex)
+            {
+                textBoxValue.Text = "EXCEPTION";
+            }
         }
 
         private void buttonStartStop_Click(object sender, EventArgs e)
         {
-            if (timer.Enabled)
+            if (timerNoise.Enabled)
             {
-                timer.Stop();
+                timerNoise.Stop();
                 buttonStartStop.Text = "Start";
             }
             else
             {
-                timer.Start();
+                timerNoise.Start();
                 buttonStartStop.Text = "Stop";
+            }
+        }
+
+        private async void timerHive_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                var hive = await client.GetFromJsonAsync<BeeLive.Hive.TransferModels.HiveDto>(ConfigurationManager.AppSettings["HiveUrl"] + "/1");
+                if (hive == null)
+                {
+                    textBoxHiveNoise.Text = string.Empty;
+                    panelColor.BackColor = Color.Transparent;
+                }
+                else
+                {
+                    textBoxHiveNoise.Text = hive.FormattedDecibel;
+                    switch (hive.Status)
+                    {
+                        case BeeLive.Hive.TransferModels.HiveStatus.Ok:
+                            panelColor.BackColor = Color.Green;
+                            break;
+                        case BeeLive.Hive.TransferModels.HiveStatus.Warning:
+                            panelColor.BackColor = Color.Yellow;
+                            break;
+                        case BeeLive.Hive.TransferModels.HiveStatus.Alarm:
+                            panelColor.BackColor = Color.Red;
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                panelColor.BackColor = Color.Transparent;
+                textBoxHiveNoise.Text = "EXCEPTION";
+            }
+        }
+
+        private void buttonCheckStart_Click(object sender, EventArgs e)
+        {
+            if (timerHive.Enabled)
+            {
+                timerHive.Stop();
+                buttonCheckStart.Text = "Start";
+            }
+            else
+            {
+                timerHive.Start();
+                buttonCheckStart.Text = "Stop";
             }
         }
     }
